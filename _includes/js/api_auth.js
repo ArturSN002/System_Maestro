a8bb879d8641e2ed0fd8e4d25330f4c5e307a314
Artur S.N. <80295771+ArturSN002@users.noreply.github.com>
2026-05-21 00:46:16 -0300

    Update api_auth.js

--- _includes/js/api_auth.js
+++ _includes/js/api_auth.js
@@ -13,7 +13,7 @@ const IAM_STATE = {
 };
 
 const CLIENT_DIRECTORY = {
-  "Ceará-Mirim": "https://script.google.com/macros/s/AKfycbzDTzpCnjgOeRqiZnh6y8zz8Kta_-GfFzP2Drr6_auiC7kctJJXbdPaEB3G-Mh6Eeeu2w/exec",
+  "Ceará-Mirim": "https://script.google.com/macros/s/AKfycbwJ1BCfh6raEGSkps0tj6pYLbOISWtEJtVVVugREyh2apHDo8h4vySrjCEGINZAO0rvfg/exec",
 };
 
 async function checkClientGateway() {
